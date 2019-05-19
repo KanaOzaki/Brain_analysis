10fe38b9c26317ff9ff2e60e1c3346b1aaecf062
KanaOzaki <31536464+KanaOzaki@users.noreply.github.com>
2019-05-19 16:21:28 +0900

    コメント追加

--- src/eval_ESRM.py
+++ src/eval_ESRM.py
@@ -1,12 +1,19 @@
 #!=usr/bin/python
 #-*- coding: utf-8 -*-
-#推定意味表象行列と正解意味表象行列とのcos類似度を計算
+#eval_ESRM.py
+
+##このプログラムは、推定意味表象行列と正解意味表象行列とのcos類似度を計算し、
+##マクロ平均を出す.
+##この時、time lagを考慮して正解と比較する.
+##また、すべてのサンプルにおけるcos類似度をpickleで保存しておく.
+##コマンド入力
+##1.被験者名 2.予測精度の閾値 3.基底数 4.time lag 5.間引き数(何sampleに1枚抜くか)
+
 import numpy as np
 import time
 import pickle
 import matplotlib.pyplot as plt
 import sys
-
 
 
 def main():
@@ -21,8 +28,8 @@ def main():
 	# 正解意味表象行列の読み込み
 	Correct_SRM = np.load("../data/srm/VB_srm300.pickle")
 
+	# 推定意味表象行列の読み込み
 	Estimate_SRM = np.load("../data/Test/VB/ESRM_pred" + threshold + "_basis" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle")
-
 	Estimate_SRM = Estimate_SRM.T
 
 	answer_list = []
@@ -35,6 +42,7 @@ def main():
 		test_sample_num = int(300 - shift/2)
 		shift_num = int(shift/2)
 
+	# time lagを考慮して正解とのcos類似度をサンプルごとに求めてリストに
 	for i in range(0,test_sample_num):
 		estimate = Estimate_SRM[i+shift_num]
 		correct = Correct_SRM[i]
@@ -49,11 +57,6 @@ def main():
 	# サンプルごとのcos類似度保存
 	with open("../data/cos_sim/cos_sim" + sub + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle", "wb") as f:
 		pickle.dump(answer_list,f)
-
-	# 0~29サンプルまでをプロット
-	#plt.plot(answer_list[:30])
-	#plt.show()
-	#plt.savefig("../data/cossim_0-30.png")
 
 
 if __name__ == '__main__':
