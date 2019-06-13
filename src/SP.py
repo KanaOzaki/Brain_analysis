3d23de82e68717a62801870f1ca4f5c544a164c4
KanaOzaki <31536464+KanaOzaki@users.noreply.github.com>
2019-06-13 15:51:39 +0900

    SP modify

--- src/SP.py
+++ src/SP.py
@@ -5,7 +5,7 @@
 ##このプログラムは、テスト用脳活動行列をスパースコーディングし、
 ##得られた係数に意味表象辞書をかけて推定意味表象行列を作成する.
 ##コマンド入力
-##1.被験者名 2.予測精度の閾値 3.基底数 4.time lag 5.間引き数(何sampleに1枚抜くか)
+##1.VB/TV 2.被験者名 3.予測精度の閾値 4.基底数 5.time lag 6.間引き数(何sampleに1枚抜くか)
 
 
 
@@ -24,21 +24,22 @@ def main():
     start = time.time()
 
     args = sys.argv
-    sub = args[1]
-    threshold = args[2]
-    dimention = int(args[3])
-    shift = int(args[4])
-    sample = int(args[5])
+    target = args[1]
+    sub = args[2]
+    threshold = args[3]
+    dimention = int(args[4])
+    shift = int(args[5])
+    sample = int(args[6])
 
     #テスト用脳活動行列の読み込み
-    with open('../data/Brain/VB/' + sub + '_test_reduced_' + threshold +'.pickle', 'rb') as f:
+    with open('../data/Brain/' + target + '/' + sub + '_test_reduced_' + threshold +'.pickle', 'rb') as f:
         test_brain_data = pickle.load(f)
 
     print("テスト脳活動行列")
     print(test_brain_data.shape)
 
     # 辞書データ読み込み(学習のとき作ったやつ)
-    Dict = np.load("../data/Dict/VB/Dict_pred" + threshold + "_basis" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle")
+    Dict = np.load("../data/Dict/" + target + "/Dict_" + sub + "_pred" + threshold + "_base" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle")
     # 脳活動辞書の部分だけ取り出す
     print(Dict.shape[1]-300)
     Brain_dict = Dict.T[0:Dict.shape[1]-300]
@@ -61,7 +62,7 @@ def main():
     print(test_coef.shape)
 
     #係数保存
-    f = open("../data/Test/VB/TestCoef_pred" + threshold + "_basis" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle","wb")
+    f = open("../data/Test/" + target + "/TestCoef_" + sub + "_pred" + threshold + "_base" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle","wb")
     pickle.dump(test_coef, f)
     f.close()
 
@@ -71,7 +72,7 @@ def main():
     print(estimated_SRM.shape)
 
     #推定意味表象行列保存
-    f = open("../data/Test/VB/ESRM_pred" + threshold + "_basis" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle","wb")
+    f = open("../data/Test/" + target + "/ESRM_" + sub + "_pred" + threshold + "_base" + str(dimention) + "_sec" + str(shift) + "_sample" + str(sample) + ".pickle","wb")
     pickle.dump(estimated_SRM, f)
     f.close()
 
